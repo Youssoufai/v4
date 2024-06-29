e7fec6514ffa5f24fed4a499b1d3af64df8b5e16
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-06-29 19:34:01 +0100

    Building

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -16,18 +16,18 @@ const Services = () => {
     return (
         <>
             <section
-                className='bg-[#f8eed3] text-black p-9 space-y-8'
+                className='bg-[#f8eed3] text-black p-9 space-y-8 h-full'
             >
-                <h1 className='text-4xl text-center mx-[27rem] m-4'
+                <h1 className='text-4xl text-center m-4'
                     style={{
                         lineHeight: '40px'
                     }}
                 >Stay updated with stories from our team</h1>
                 <section
-                    className='flex justify-center gap-8'
+                    className='flex flex-col md:flex-row items-center justify-center gap-8'
                 >
                     <div
-                        className='p-4'
+                        className='p-4 space-y-8 text-center flex justify-center flex-col items-center'
                     >
                         <div
                             className='w-[333.32px] h-[234.66]'
@@ -36,9 +36,13 @@ const Services = () => {
                                 className='w-full'
                                 alt="" />
                         </div>
+                        <h3 className='text-blue'>Stories from the field</h3>
+                        <h1 className='font-bold text-2xl'>The Journey of Your Donation</h1>
+                        <p>Watch this video to see how your donation turns into water, sanitation, and hygiene resources that transform entire communities and impact more people than you know.</p>
+                        <a href="#">Learn More</a>
                     </div>
                     <div
-                        className='p-4'
+                        className='p-4 space-y-8 text-center flex justify-center flex-col items-center'
                     >
                         <div
                             className='w-[333.32px] h-[234.66]'
@@ -47,9 +51,15 @@ const Services = () => {
                                 className='w-full'
                                 alt="" />
                         </div>
+                        <h3 className='text-blue'>Stories from the field</h3>
+                        <h1 className='font-bold text-2xl'>The Journey of Your Donation</h1>
+                        <p>Watch this video to see how your donation turns into water, sanitation, and hygiene resources that transform entire communities and impact more people than you know.</p>
+                        <a href="#"
+                            className='relative after:absolute after:bg-gold after:h-3 after:w-[100%]'
+                        >Learn More</a>
                     </div>
                     <div
-                        className='p-4'
+                        className='p-4 space-y-8 text-center flex justify-center flex-col items-center'
                     >
                         <div
                             className='w-[333.32px] h-[234.66]'
@@ -58,6 +68,12 @@ const Services = () => {
                                 className='w-full'
                                 alt="" />
                         </div>
+                        <h3 className='text-blue'>Stories from the field</h3>
+                        <h1 className='font-bold text-2xl'>The Journey of Your Donation</h1>
+                        <p>Watch this video to see how your donation turns into water, sanitation, and hygiene resources that transform entire communities and impact more people than you know.</p>
+                        <a href="#"
+                            className='relative after:absolute after:bg-gold after:h-3 after:w-[100%]'
+                        >Learn More</a>
                     </div>
                 </section>
             </section>
