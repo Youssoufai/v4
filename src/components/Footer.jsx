817f9ccbc615abff53cf159db1d20b0edd32ca90
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-07-01 20:50:29 +0100

    Building

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ import { FaFacebook } from 'react-icons/fa';
 const Footer = () => {
     return (
         <>
-            <section className='p-12 h-full space-y-8 bg-[#f7f7f7] text-white flex'>
+            <section className='p-12 h-full space-y-8 bg-[#f7f7f7] text-white flex flex-col md:flex-row'>
                 <div
-                    className='text-black text-start space-y-8 w-[50%] flex justify-center flex-col'
+                    className='text-black text-start space-y-8 md:w-[50%] flex justify-center flex-col'
                 >
                     <h1 className='text-4xl font-bold text-start'>Help us improve</h1>
                     <p>We want to hear from you! In this 5-minute survey, we'll ask you to weigh in on our 100% model and how you feel about the different ways you can support a charity.</p>
@@ -17,8 +17,8 @@ const Footer = () => {
                         <a href="#" className='bg-[#ffca0a] px-6 py-3'>TAKE OUR SURVEY</a>
                     </button>
                 </div>
-                <div className='w-[50%] '>
-                    <img src={g} className='h-[30rem] rounded' alt="" />
+                <div className='md:w-3/6 '>
+                    <img src={g} className='h-[30rem] wrounded' alt="" />
                 </div>
             </section>
         </>
--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import water from '../assets/water.webp';
-import water2 from '../assets/water2.webp';
+import cloth from '../assets/cloth.jpg';
 const Help = () => {
     return (
         <>
@@ -13,11 +13,36 @@ const Help = () => {
                 <p
                     className='md:mx-[12rem] text-center text-black'
                 >At Versalink, we believe in the power of community and collaboration. Whether you're a tech enthusiast, a business professional, or someone passionate about making a difference, there are numerous ways for you to get involved with our mission. Join us in shaping the future of technology and services.</p>
-                <div>
-                    <div>
+                <div
+                    className='flex gap-8'
+                >
+                    <div
+                        className='flex gap-8 bg-[#fff] rounded-r-lg w-[40rem]'
+                    >
                         <img src={water}
                             className='h-[220px] w-[250px] rounded-l-lg'
                             alt="" />
+                        <div className='text-black flex flex-col justify-evenly'>
+                            <h1 className='text-2xl'>Fundraiser for clean water</h1>
+                            <p>Creating a campaign helps raise money for clean water projects.</p>
+                            <a href="#"
+                                className='relative after:absolute after:bg-gold after:h-1 after:left-0 after:rounded after:w-[40%] after:bottom-0'
+                            >Learn More</a>
+                        </div>
+                    </div>
+                    <div
+                        className='flex gap-8 bg-[#fff] rounded-r-lg w-[40rem]'
+                    >
+                        <img src={cloth}
+                            className='h-[220px] w-[250px] rounded-l-lg'
+                            alt="" />
+                        <div className='text-black flex flex-col justify-evenly'>
+                            <h1 className='text-2xl'>Patronise our store</h1>
+                            <p>Creating a campaign helps raise money for clean water projects.</p>
+                            <a href="#"
+                                className='relative after:absolute after:bg-gold after:h-1 after:left-0 after:rounded after:w-[40%] after:bottom-0'
+                            >Learn More</a>
+                        </div>
                     </div>
                 </div>
             </section>
